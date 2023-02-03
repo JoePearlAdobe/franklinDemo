bc7d32bc04e2ed5aca0f20f3bc16a461a1aa07cf
JoePearlAdobe <42250657+JoePearlAdobe@users.noreply.github.com>
2023-02-03 10:17:26 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -60,19 +60,7 @@ function buildAutoBlocks(main) {
   }
 }
 
-/**
- * Decorates the main element.
- * @param {Element} main The main element
- */
-// eslint-disable-next-line import/prefer-default-export
-export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
-}
+
 
 /**
  * loads everything needed to get to LCP.
